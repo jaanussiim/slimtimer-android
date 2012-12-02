83ea626fa1bcca9ab40928c16639fdb68fb5211f
JaanusSiim <jaanus@jaanussiim.com>
2012-12-02 17:03:22 +0100

    Empty project with robolectric

---
+++ tests/com/jaanussiim/slimtimer/SlimtimerActivityTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2012 JaanusSiim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jaanussiim.slimtimer;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class SlimtimerActivityTest {
+  @Test
+  public void shouldHaveHappySmiles() throws Exception {
+    String appName = new SlimtimerActivity().getResources().getString(R.string.app_name);
+    assertThat(appName, equalTo("mTimer"));
+  }
+}
